32784928fdf509821d94dfd66db599637916e36c
sunnyghosh108 <sunnyghoshindra009@gmail.com>
2023-05-03 23:09:51 +0600

    error page create

--- src/Components/Error/Error.jsx
+++ src/Components/Error/Error.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import './Error.css';
+ import error from '../../assets/error.jpg'
 const Error = () => {
     return (
-        <div>
-            <h3>Error</h3>
-        </div>
+        
+            <div className="not-found-container">
+      <h1 className="not-found-header">Oops! Page not found</h1>
+      <img className='error-img' src={error} alt="" />
+      <p className="not-found-text">We're sorry, but the page you requested could not be found.</p>
+
+      </div>
     );
 };
 
